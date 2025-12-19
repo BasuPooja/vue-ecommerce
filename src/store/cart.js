0e846ce32f9448f9f0c816756ad7ec78f045d3c6
Pooja Kumari <basupooja306@gmail.com>
2025-12-19 23:41:26 +0530

    Cart System Using Vuex change into option api

--- src/store/cart.js
+++ src/store/cart.js
@@ -3,7 +3,8 @@ import { createStore } from "vuex";
 export default createStore({
     state(){
         return{
-            cart:[]
+            cart:[],
+            searchQuery: ""
         };
     },
     mutations:{
