5739b1f284cba08d00e155120bf5e9c8519754db
Pooja Kumari <basupooja306@gmail.com>
2025-12-22 23:30:08 +0530

    conflict update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@
             placeholder="Search products..."
             class="flex-1 px-5 py-2.5 border border-gray-300 rounded-l-full
                    focus:outline-none focus:ring-2 focus:ring-pink-500"
-
+            
           />
           <button
             class="px-6 bg-pink-600 text-white rounded-r-full
